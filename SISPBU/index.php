298c019b9da37f64b9791b619f39026c1f06594d
tika <tika.felish@gmail.com>
2015-04-03 20:31:26 +0700

    Delete index.php

--- SISPBU/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (ISSET($_SESSION['sesi'])){?>
-    <script language="JavaScript">document.location='daftar.php';</script><?php
-} else {
-    header('location: login.php');
-}
-?>
